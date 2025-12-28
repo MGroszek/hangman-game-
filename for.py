78ea36e046a3253f360a0cc079ff2c3882a9f6cf
Mateusz Grochowalski <matigroszek4@gmail.com>
2025-12-28 19:17:21 +0100

    Keep only hangman.py

--- for.py
+++
@@ -1,7 +0,0 @@
-player_name = ["Leo Messi", "Cristiano Ronaldo ",
-               "Robert Lewandowski ", "Tom Halland"]
-for x in range(4):
-    if x > 1:
-        break
-    print("Player " + str(x+1) + " : " + player_name[x])
-print("The END")
